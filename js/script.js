418cca31ead7199e73fb75f325a96e31c3f66fa6
MOLOREXON <editllise@gmail.com>
2018-10-10 18:45:25 +0200

    +HD Alex +добавлены title для изображений и меню

--- js/script.js
+++ js/script.js
@@ -1,9 +1,25 @@
-var imagesArray =   [   'Patrick_Circle',
-                        'Alex_Circle'
-                    ];
-
 function displayImage(){
-    var num = Math.floor(Math.random() * 2); // 0...6
-    document.getElementById('mainCircleMemeID').src     = 'img/mainPage/'+ imagesArray[num] + '.png';
-    document.getElementById('mainCircleMemeTopID').src  = 'img/mainPage/'+ imagesArray[num] + '_Top.png';
+    var num = Math.floor(Math.random() * 2);
+    var source_image = "";
+    var source_title = "";
+
+    switch(num){
+
+        case 0:
+            source_image = "Patrick_Circle";
+            source_title = "SAVAGE PATRICK";
+        break;
+        
+        case 1:
+            source_image = "Alex_Circle";
+            source_title = "AMA FAGGOT";
+        break;
+
+    }
+
+    document.getElementById('mainCircleMemeID').src     = 'img/mainPage/'+ source_image + '.png';
+    document.getElementById('mainCircleMemeTopID').src  = 'img/mainPage/'+ source_image + '_Top.png';
+
+    document.getElementById('mainCircleMemeID').title   = source_title;
+    document.getElementById('mainCircleMemeTopID').title= source_title;
 }
